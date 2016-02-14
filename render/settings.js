70d444bffa802eae619a0fe22b07c1a9b25b5714
clement oury <ourycl@gmail.com>
2016-02-14 16:55:42 +0100

    conf

--- render/settings.js
+++ render/settings.js
@@ -1,8 +1,8 @@
 // Barcode view settings
-var display_groups = [];
+var display_groups = ["browsing", "fpc-programming", "misc", "music", "processing", "read-rss", "compta", "mail", "social"];
 // Productive
-display_groups.push(["read-djvu", "read-pdf", "read-epub", "read-mobi", "code-web", "code-python", "code-haskell", "code-ruby", "code-matlab", "write", "term", "mail"]);
+display_groups.push(["mail", "fpc-programming", "processing", "compta");
 // Unproductive
-display_groups.push(["irc", "social", "communities", "tv-local", "tv-web", "read-rss"]);
+display_groups.push(["social", "music", "read-rss"]);
 // Don't know
 display_groups.push(["misc", "browsing"]);
